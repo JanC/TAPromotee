9c84e92b3165964114a65199f227aa384411f160
Jan Chaloupecky <jan.chaloupecky@gmail.com>
2015-04-29 14:42:15 +0200

    added some documentation

--- Pod/Classes/TAITunesClient.h
+++ Pod/Classes/TAITunesClient.h
@@ -8,5 +8,14 @@ typedef void (^TAAppInfoCompletionBlock)(NSDictionary *response, NSError *error)
 
 @interface TAITunesClient : NSObject
 
+/**
+ *  Fetches the json information about a specified iOS App from iTunes. See
+ *  https://www.apple.com/itunes/affiliates/resources/documentation/itunes-store-web-service-search-api.html#searching
+ *
+ *  @param appId      The App Store app id
+ *  @param country    The two-letter country code for the store you want to search.
+ *  @param completion The completion block that contains the json dictionary of the app
+ */
 - (void)fetchInfoForAppId:(NSInteger)appId country:(NSString *)country completion:(TAAppInfoCompletionBlock)completion;
+
 @end
